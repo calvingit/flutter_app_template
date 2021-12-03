a5c7a2f5c22141619d121d27c330f1234fd4af2b
张文 <wen.zhang@legrand.com.cn>
2021-12-03 17:14:25 +0800

    add .env

--- lib/utils/dependency_injection.dart
+++ lib/utils/dependency_injection.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app_template/services/storage.dart';
 import 'package:get/get.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DependencyInjection {
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
+    await dotenv.load(fileName: ".env");
 
     await Get.putAsync(() => StorageService().init());
   }
